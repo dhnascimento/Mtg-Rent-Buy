814bb968566db1ee1ba6e4ca1e83a59cbdb33456
Daniel Nascimento <danielhn@gmail.com>
2021-01-04 16:08:04 -0500

    Global App Controller initial logic

--- app.js
+++ app.js
@@ -3,5 +3,24 @@
 // UI Controller
 
 //Global App Controller
+const controller = (function (UICtrl) {
+  const setupEventListeners = function () {
+    const DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  const ctrlAddItem = function () {};
+
+  return {
+    init: function () {
+      setupEventListeners();
+    },
+  };
+})(UIController);
 
 controller.init();
