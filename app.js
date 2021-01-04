c77cb69f0b31bb281f558da3284db5c843ebab00
Daniel Nascimento <danielhn@gmail.com>
2021-01-04 16:36:24 -0500

    Added owning case input vars to DOMstrings object

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 //Business Logic Controller
 
 // UI Controller
+const UIController = function () {
+  //Getting the variables from the input fields
+  const DOMstrings = {
+    inputBtn: ".add__btn",
+    inputHouseValue: ".add__house_value",
+    inputDownPayment: ".add__down_payment",
+    inputRate: ".add__interest_rate",
+    inputAmort: ".add__amort_period",
+    inputIsToronto: ".add__is_toronto",
+    inputTitleInsurance: ".add__title_insurance",
+    inputLegalFees: ".add__legal_fees",
+    inputHomeInspection: ".add__home_inspection",
+    inputComissionRate: ".add__comission_rate",
+    inputMaintenanceRate: ".add__maitenance_rate",
+    inputPropertyTax: ".add__property_tax",
+    inputHouseInsurance: ".add__house_insurance",
+    inputAppreciationRate: ".add__appreciation_rate",
+  };
+};
 
 //Global App Controller
 const controller = (function (UICtrl) {
