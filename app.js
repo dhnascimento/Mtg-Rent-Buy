f75a02ef11c419a3fa306e0661e4edcf89d430d6
Daniel Nascimento <danielhn@gmail.com>
2021-02-01 23:11:39 -0500

    Changes in html, start code for validation

--- app.js
+++ app.js
@@ -1131,6 +1131,15 @@ const controller = (function (UICtrl) {
   const setupEventListeners = function () {
     const DOM = UICtrl.getDOMstrings();
     document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    // Form control trigger
+    // var form = document.querySelector("form");
+    // var triggerButton = document.querySelector("button");
+
+    // triggerButton.onclick = function () {
+    //   form.reportValidity();
+    // };
+
     document.addEventListener("keypress", function (event) {
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
